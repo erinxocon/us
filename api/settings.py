952cc41c227e03a90478a994c9c59ec747179de4
Erin O'Connell <erinocon5@gmail.com>
2020-08-17 11:03:11 -0600

    August 17, 2020 11:03 AM

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,42 @@
+from secrets import token_urlsafe
+from typing import Any, Dict, Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, HttpUrl, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
-    ...
+    API_V1_STR: str = "/v1"
+    SECRET_KEY: str = token_urlsafe(32)
+
+    SENTRY_DSN: Optional[HttpUrl] = None
+
+    @validator("SENTRY_DSN", pre=True)
+    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
+        if len(v) == 0:
+            return None
+        return v
+
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any],) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+
+if __name__ == "__main__":
+    s = Settings()
+    print(s)
