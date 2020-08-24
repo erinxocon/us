adf66e2d32cb183e0ac915358d66da075aefc3df
Erin O'Connell <erinocon5@gmail.com>
2020-08-23 18:37:28 -0600

    big reorg because I can't decide what I want, and I really need to get migrations working

--- api/app/models.py
+++ api/app/models.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
+from .db.base_class import Base
+
 
 class User(Base):
-    __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
@@ -14,8 +14,6 @@ class User(Base):
 
 
 class Item(Base):
-    __tablename__ = "items"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
